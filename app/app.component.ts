77ac71fc2733862a848390fbed9aa338c2283aa4
GeorgeKrystev <george.krystew@gmail.com>
2016-07-05 19:30:05 +0300

    added jumbotron componet

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component'
+import { JumbotronComponent } from './components/jumbotron/jumbotron.component'
 
 @Component({
     selector: 'my-app',
     template: `
         <navbar></navbar>
+        <jumbotron></jumbotron>
         <h1>My First Angular 2 App</h1>
        
     `,
-    directives:[NavbarComponent] 
+    directives:[NavbarComponent, JumbotronComponent] 
 })
 export class AppComponent { }
---
+++ app/components/jumbotron/jumbotron.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'jumbotron',
+    templateUrl: 'jumbotron.component.html'
+})
+export class JumbotronComponent { 
+    private jbtHeading: string;
+    private jbtTxt: string;
+    private jbtBtnTxt: string;
+    private jbtBtnUrl: string;
+    
+    constructor(){
+        this.jbtHeading = 'Hello, world!'
+        this.jbtTxt = `This is a template for a 
+                simple marketing or informational website. 
+                It includes a large callout called a jumbotron 
+                and three supporting pieces of content. Use it as a 
+                starting point to create something more unique.`;
+        this.jbtBtnTxt = 'Learn more »'
+        this.jbtBtnUrl = '#'
+    }
+}
